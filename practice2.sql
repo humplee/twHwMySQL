13ef109b3f27aa00b5a59fe4f89e355b086481fb
humplee <littleted1221@163.com>
2019-06-20 22:25:02 +0800

    practice two

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,8 @@
 # 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+
+select companyName,eName from(
+select companyName,eName,max(salary)
+from Company,Employee
+where Company.id=Employee.companyId
+group by Company.id)as a
