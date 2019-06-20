00eee23dab1cd14a77fadcbeecf96c9e2ca3bd9d
humplee <littleted1221@163.com>
2019-06-20 14:08:29 +0800

    p1

--- practice1.sql
+++ practice1.sql
@@ -11,3 +11,26 @@
 # 6.找出Employee表中姓名包含`n`字符并且薪资大于6000的雇员所有个人信息
 
 # ps:本练习中只需要将6的结果复制到`result.txt`文件中
+
+create database employee_db;
+
+use employee_db;
+
+create table Employee(
+id int primary key,
+eName char(40),
+age int,
+gender char(10),
+companyId int,
+salary int 
+);
+
+create table Company(
+    id int PRIMARY KEY,
+    companyName char(100),
+    employeesNumber int
+);
+
+LOAD DATA LOCAL INFILE '/var/sources/company-data.csv' INTO TABLE Company FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' (id, companyName, employeesNumber);
+
+select * from Employee where eName like '%n%' and salary>6000;
