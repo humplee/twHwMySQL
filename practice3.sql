184688c2770ce4f2e77411521c934df3c6473b70
humplee <littleted1221@163.com>
2019-06-20 22:50:23 +0800

    practice three and four

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,9 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
+select companyName,avg(Salary) as avgs
+from Company,Employee
+where Company.id=Employee.companyId
+group by company.id
+order by avg(Salary) desc limit 1
 
--- practice4.sql
+++ practice4.sql
@@ -1,3 +1,9 @@
 # 找出薪水在公司的平均薪水之上的人员名字
 # 输出标准中包含Employee中所有字段和该employee所在公司名称以及该公司平均薪资：
 # id | name | age | gender | companyId | salary | companyName | avgsal
+select Employee.id, Employee.eName,Employee.gender, Employee.companyId, Employee.salary, companyName, CompanyAvgSalary.avgs from
+(select companyName,companyId,avg(Salary) as avgs
+from Company,Employee
+where Company.id=Employee.companyId
+group by company.id) as CompanyAvgSalary,Employee
+where CompanyAvgSalary.companyId=Employee.companyId and Employee.salary>=CompanyAvgSalary.avgs
